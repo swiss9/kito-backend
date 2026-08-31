cce83b5a12245acd36a199b9cb34d5ce3825ef8a
swiss <101133333+swiss9@users.noreply.github.com>
2026-08-31 17:28:47 +0630

    Create logger.js

---
+++ services/logger.js
@@ -0,0 +1,11 @@
+const pino = require('pino');
+
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'info',
+  formatters: {
+    level: (label) => ({ level: label }),
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+});
+
+module.exports = logger;
