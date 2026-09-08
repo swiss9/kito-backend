8e5ce9810c96e91d92f7a83cf31392b91512197f
swiss <101133333+swiss9@users.noreply.github.com>
2026-09-09 01:40:00 +0630

    Add Redis client initialization in redisClient.js

---
+++ services/redisClient.js
@@ -0,0 +1,11 @@
+const { Redis } = require('@upstash/redis');
+
+let redisClient = null;
+if (process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN) {
+  redisClient = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  });
+}
+
+module.exports = redisClient;
